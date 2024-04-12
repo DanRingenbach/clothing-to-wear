8f6e8d127778bdd36f7f52692e3f971c05125e8b
Dan Ringenbach <danringenbach.art@gmail.com>
2024-04-12 14:28:40 -0400

    mobile fixes

--- components/ImageOverlay.tsx
+++ components/ImageOverlay.tsx
@@ -20,8 +20,8 @@ const ImageOverlay: React.FC<ImageOverlayProps> = ({ imageUrl }) => {
       <Image src={imageUrl} width={400} height={400} alt="Click to view" onClick={toggleOverlay} />
       {isOpen && (
         <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center" onClick={toggleOverlay}>
-          <div className="max-w-1/2 h-[calc(100vh)]">
-            <Image src={imageUrl} width={400} height={400} alt="Overlay" className="w-full" />
+          <div className="max-w-[calc(100vw)] lg:h-[calc(100vh)] md:h-[calc(100vh)] sm:h-[calc(80vh)]">
+            <Image src={imageUrl} width={400} height={400} alt="Overlay" className="w-full lg:h-full md:h-full sm:h-auto"/>
           </div>
         </div>
       )}
